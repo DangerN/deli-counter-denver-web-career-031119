0f7e723cce5bcd249ef27464fc8e6ce1f7f67bf5
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 01:59:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
 # Write your code here.
+def line(arr)
+  if !arr.empty?
+    line =  "The line is currently:"
+    arr.each.with_index {|pers,i| line << " #{i+1}. #{pers}"}
+    puts line
+  else
+    puts "The line is currently empty."
+  end
+end
